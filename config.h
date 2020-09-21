b8ec7b08218a6221d06048206fac1771c2807ee9
Gabriel Nogueira <gab.nog94@gmail.com>
2020-09-21 18:14:55 -0300

    change shortcuts for last patch added

--- config.h
+++ config.h
@@ -125,8 +125,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = +1 } },
 	{ Mod1Mask|ShiftMask,           XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_comma,  tagswapmon,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_period, tagswapmon,     {.i = -1 } },
+	{ Mod1Mask|ShiftMask,           XK_comma,  tagswapmon,     {.i = +1 } },
+	{ Mod1Mask|ShiftMask,           XK_period, tagswapmon,     {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
