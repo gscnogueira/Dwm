7ac58487b136e3252d5d35c975ec7138890137a0
Gabriel Nogueira <gab.nog94@gmail.com>
2020-10-28 03:55:40 -0300

    Changed st for alacritty

--- config.def.h
+++ config.def.h
@@ -71,9 +71,9 @@ void swaptags(const Arg *arg);
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "60x17", NULL };
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "-d", "60","17",NULL };
 static const char *bravecmd[] = { "brave", NULL };
 static const char *chromiumcmd[] = { "chromium", NULL };
 static const char *incognitobravecmd[] = { "brave", "--incognito",NULL };
